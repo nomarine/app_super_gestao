45f26dcdb4c5780943a5f8fdbac8cdd4bf34daca
nomarine <rafaelfijos@gmail.com>
2021-07-05 21:01:35 -0300

    Condicionais

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -8,11 +8,16 @@ class FornecedoresController extends Controller
 {
     public function index() {
         $fornecedores = [
-            'EDP',
-            'Sabesp',
-            'Tanby',
-            'Peloggia'
+            0 => ['nome' => 'EDP', 'status' => 'A'],
+            1 => ['nome' => 'Sabesp', 'status' => 'A'],
+            2 => ['nome' => 'Tanby', 'status' => 'I'],
+            3 => ['nome' => 'Peloggia', 'status' => 'I', 'cnpj' => '50.127.889.01-0001/78'],
+            4 => ['nome' => 'Ranne Informática', 'status' => 'A', 'cnpj' => '']
         ];
+
+        $fornecedor_status = $fornecedores[4]['status'] == 'I' ? 'Fornecedor inativo' : 'ATIVO';
+        echo $fornecedor_status;
+
         return view('app.fornecedores', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedores.blade.php
+++ resources/views/app/fornecedores.blade.php
@@ -2,10 +2,30 @@
 
 {{-- @dd($fornecedores) --}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores</h3> 
 @elseif(count($fornecedores) > 10)
     <h3>Existem vários fornecedores</h3>
 @else
     <h3>Não existem fornecedores cadastrados</h3>
-@endif
+@endif --}}
+
+@isset($fornecedores[4])
+    Fornecedor: {{ $fornecedores[4]['nome'] }}
+    @unless($fornecedores[4]['status'] == 'A')
+        <br>
+        Status: {{ $fornecedores[4]['status'] }}
+    @endunless
+    @isset($fornecedores[4]['cnpj'])
+        <br>
+        CNPJ: 
+        @empty($fornecedores[4]['cnpj'])
+                Não informado
+            @else
+                {{ $fornecedores[4]['cnpj'] }}
+        @endempty
+    @endisset
+@endisset
+
+{{-- @unless($fornecedores) --}}
+
