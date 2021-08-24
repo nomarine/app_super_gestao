d1ace1dd5210ee4a6ba430929bef00f90c24f618
nomarine <rafaelfijos@gmail.com>
2021-08-24 12:13:11 -0300

    Cabeçalho e rodapé separados

---
+++ app/Http/Controllers/ExpController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExpController extends Controller
+{
+    public function heranca() {
+        return view('exp.index');
+    }
+}
