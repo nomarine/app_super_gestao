5f4bf2c64c9599625d073eaa44be2d0b344dde4b
nomarine <rafaelfijos@gmail.com>
2021-08-24 12:53:12 -0300

    Arrumado include dos layouts

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,23 +1,8 @@
-@extends('site.layouts.rodape')
+@extends('site.layouts.basico') 
 
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
@@ -46,4 +31,5 @@
             </div>
         </div>  
     </div>
+    @include('site.layouts._partials.rodape')
 @endsection
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,23 +1,8 @@
-@extends('site.layouts.rodape')
+@extends('site.layouts.basico')
 
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
-
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
             <h1>Olá, eu sou o Super Gestão</h1>
@@ -28,4 +13,5 @@
             <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
         </div>  
     </div>
+    @include('site.layouts._partials.rodape')
 @endsection
