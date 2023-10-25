0421518e39b4ec4fa3e12cec900754304cd2afdb
nomarine <rafaelfijos@gmail.com>
2023-10-25 12:16:11 -0300

    Change mapped Model from Produto to Item on Pedido Model

--- app/Pedido.php
+++ app/Pedido.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     public function produtos() {
-        return $this->belongsToMany('\App\Produto', 'pedidos_produtos');
+        return $this->belongsToMany('\App\Item', 'pedidos_produtos', 'pedido_id', 'produto_id');
     }
 }
